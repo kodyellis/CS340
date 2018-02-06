d8f72b48d618d88129def4ec019ec1053a97c2c2
Kody Ellis <kellis5099@tuskegee.edu>
2018-02-05 21:59:45 -0600

    Add files via upload
    final versions that I submit for grading

--- Order.java
+++ Order.java
@@ -1,171 +1,173 @@
-package lab2;
+// Kody Ellis Order.java Tuskegee University Database CS 340 class, Submission date: Feb5, 2018
 
 import java.io.*;
-//import java.util.*;
 
 //importing 4 more because thats better than importing all of util.
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Scanner;
 //legacy package, only used because ease-of-access for this particular assignment.
 import java.util.StringTokenizer;
-import java.util.Scanner;
-
-
-//FYI: Can make an instance var/method that holds a number for each pizze
-//like pizza1, pizza2, etc
 
 /**
  * Represents a pizza order, comprised of one or more pizzas.
  * */
 public class Order {
 
-	//static allows this to be sued through the whole class
-	//since it is a variable of a class instance
-	public static ArrayList<Pizza> pizzaList =  new ArrayList<Pizza>();
-	
+   //static allows this to be used through the whole class
+   //since it is a variable of a class instance
+   public static ArrayList<Pizza> pizzaList =  new ArrayList<Pizza>();
 
-	public static void main(String[] args) {
-		
-		Order thisOrder = new Order();
-		
-		thisOrder.getFile();
 
-		//sorts array list based on cost of each pizza element then rearranges based on descending order(high to low)
-		Collections.sort(pizzaList, Collections.reverseOrder());
-		
-		thisOrder.display();
-		thisOrder.getCost();
+   public static void main(String[] args) {
 
-	}
-	
-	/**
-	 * Opens target file and extracts pizza crust, size, and toppings data.
-	 * After data is extracted for 1 pizza,  switches to another function 
-	 * to add that pizza's data to the pizza arraylist.
-	 * */
-	public void getFile() {
-		String crust = "n/a";
-		String size = "n/a";
-		StringTokenizer test;
-		Order myOrder = new Order();
+      String fileName;
 
-		String[] toppings = new String[7];
-		
-		//so the scanner object is there in case the file directory is mssing
+      // is filename is not provided to program at runtime, then the program
+      //doesn't crash and filename example is provided instead.
+      try {
+
+         fileName = args[0];
+
+   } catch(Exception ex) {
+         fileName = "testFile.dat";
+      }
+
+      Order thisOrder = new Order();
+
+      thisOrder.getFile(fileName);
+
+      //sorts array list based on cost of each pizza element then rearranges based on descending order(high to low)
+      Collections.sort(pizzaList, Collections.reverseOrder());
+
+      thisOrder.display();
+      thisOrder.getCost();
+
+   }
+
+   /**
+    * Opens target file and extracts pizza crust, size, and toppings data.
+    * After data is extracted for 1 pizza,  switches to another function
+    * to add that pizza's data to the pizza arraylist.
+    * */
+   public void getFile(String fileName) {
+      String crust = "n/a";
+      String size = "n/a";
+      StringTokenizer test;
+      Order myOrder = new Order();
+
+      String[] toppings = new String[7];
+
+      //so the scanner object is there in case the file directory is mssing
         Scanner input = new Scanner("n/a");
 
-		try{
-			input = new Scanner(new File("C:\\Users\\Kody Ellis\\eclipse-workspace\\cs340 lab2\\src\\lab2\\testFile"));
-			
-		}catch (Exception ex) {
-			
-			ex.printStackTrace();
-		}
-		
-		while(input.hasNextLine()) {
-			//starts line 1, gets crust and size
-			String line = input.nextLine();
-			test = new StringTokenizer(line, ", ");
-			crust = test.nextToken();
-			size = test.nextToken();
-				
-			//starts line 2, gets each topping			
-			line = input.nextLine();
-			test = new StringTokenizer(line, ",");
+      try{
+         input = new Scanner(new File(fileName));//"C:\\Users\\Kody Ellis\\eclipse-workspace\\cs340 lab2\\src\\lab2\\testFile"));
 
-				//gets string array
-			int index = 0;
-			while(test.hasMoreTokens()){
-				toppings[index] = test.nextToken();
-				index++;
-			}
-								
-				
-			//adds to array after every 2lines(a.k.a. pizzas)
-			myOrder.addList(crust,size,toppings);
-			}
-		
-		//closes scanner 
-		input.close();
+      }catch (Exception ex) {
 
-	}
-	
-	/**
-	 * Adds  a Pizza object to a Pizza ArrayList.
-	 * @param crust This is The Pizza's crust
-	 * @param size This is the Size of the pizza. Used in
-	 * 					finding the cost of the pizza
-	 * @param toppings These are the toppings of the Pizza. Used in
-	 * 					finding the cost of the pizza.
-	 */
-	public void addList(String crust, String size, String[] toppings) {
-		//create pizza object
-		Pizza myPizza = new Pizza(crust, size);
-		myPizza.setToppings(toppings); 
-		
-		//for code stability
-		myPizza.costOfPizza();
-		
-		//add to array list
-		pizzaList.add(myPizza);
+         ex.printStackTrace();
+      }
 
-	}
+      while(input.hasNextLine()) {
+         //starts line 1, gets crust and size
+         String line = input.nextLine();
+         test = new StringTokenizer(line, ", ");
+         crust = test.nextToken();
+         size = test.nextToken();
 
-	/**
-	 * calculates cost of each pizza and gets total amount
-	 * should be changed in order to accommodate adding more pizzas from user choice
-	 */
-	public void getCost() {
-		double cost = 0;
-		double totalCost = 0;
-		
-		//Doesnt calculate cost anymore once it's been called.'
-		//should get changed if we are adding the feature  add multiple pizzas again
-	    //total cost is creating a pizza object iterating through each on gettign the cost then computing up total cost
-		if (Pizza.getTotal() == 0) {
-			for (int i = 0; i < pizzaList.size(); i++) {
-				cost = pizzaList.get(i).costOfPizza();
-				
-				//System.out.println("pizza"+i+" cost: "+cost);
-				Pizza.setTotal(cost);
-	
-			}
-		}
-		
-		System.out.println("Pizza subtotal: " + Pizza.getTotal());
-		
-		totalCost = Pizza.getTotal() + (Pizza.getTotal()*Pizza.getTax());
-		System.out.println("Pizza total: " + totalCost);
-	}
-	
-	/**
-	 * Prints out info about each pizza, like crust, size, toppings and cost info.
-	 * */
-	public void display() {
-		
-		//before sort
-		/*
-		for (int i = 0; i < pizzaList.size(); i++) {
-			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");
-			//i+1 because i starts at 0
-			System.out.println("Pizza "+(i+1)+"\n"+"Crust:"+ pizzaList.get(i).getCrust()+"\n"+"Size: "+pizzaList.get(i).getSize()+"\n"+"Toppings: "+pizzaList.get(i).getToppings()+"\n"+"Cost:"+pizzaList.get(i).costOfPizza());
-			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+         //starts line 2, gets each topping
+         line = input.nextLine();
+         test = new StringTokenizer(line, ",");
 
-		}
-		System.out.println("---------------------------------------------------");
-		System.out.println("---------------------------------------------------");
-*/
-		for (int i = 0; i < pizzaList.size(); i++) {
-			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");
-			//i+1 because i starts at 0
-			
-			//Pizza number isnt necessarily the number of the pizza when it was ordered.
-			System.out.println("Pizza "+(i+1)+"\n"+"Crust:"+ pizzaList.get(i).getCrust()+"\n"+"Size: "+pizzaList.get(i).getSize()+"\n"+"Toppings: "+pizzaList.get(i).getToppings()+"\n"+"Cost:"+pizzaList.get(i).costOfPizza());
-			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+         //gets string array
+         int index = 0;
+         while(test.hasMoreTokens()){
+            toppings[index] = test.nextToken();
+            index++;
+         }
 
-		}
-	}
-	
-	
+
+         //adds to array after every 2lines(a.k.a. pizzas)
+         myOrder.addList(crust,size,toppings);
+         }
+
+      //closes scanner
+      input.close();
+
+   }
+
+
+
+   /**
+    * Adds  a Pizza object to a Pizza ArrayList.
+    * @param crust This is The Pizza's crust
+    * @param size This is the Size of the pizza. Used in
+    *                finding the cost of the pizza
+    * @param toppings These are the toppings of the Pizza. Used in
+    *                finding the cost of the pizza.
+    */
+   public void addList(String crust, String size, String[] toppings) {
+      //create pizza object
+      Pizza myPizza = new Pizza(crust, size);
+      myPizza.setToppings(toppings);
+
+      //for code stability
+      myPizza.costOfPizza();
+
+      //add to array list
+      pizzaList.add(myPizza);
+
+   }
+
+
+
+   /**
+    * calculates cost of each pizza and gets total amount
+    * should be changed in order to accommodate adding more pizzas from user choice
+    */
+   public void getCost() {
+      double cost = 0;
+      double totalCost = 0;
+
+      //Doesnt calculate cost anymore once it's been called.'
+      //should get changed if we are adding the feature  add multiple pizzas again
+       //total cost is creating a pizza object iterating through each on gettign the cost then computing up total cost
+      if (Pizza.getTotal() == 0) {
+         for (int i = 0; i < pizzaList.size(); i++) {
+            cost = pizzaList.get(i).costOfPizza();
+
+            //System.out.println("pizza"+i+" cost: "+cost);
+            Pizza.setTotal(cost);
+
+         }
+      }
+
+      System.out.println("Pizza subtotal: " + Pizza.getTotal());
+
+      totalCost = Pizza.getTotal() + (Pizza.getTotal()*Pizza.getTax());
+      System.out.println("Pizza total: " + totalCost);
+   }
+
+
+
+
+
+
+   /**
+    * Prints out info about each pizza, like crust, size, toppings and cost info.
+    * */
+   public void display() {
+
+      for (int i = 0; i < pizzaList.size(); i++) {
+         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+         //i+1 because i starts at 0
+
+         //Pizza number isnt necessarily the number of the pizza when it was ordered,
+         //just repressents the ordererd place of the pizza in the list.
+         System.out.println("Pizza "+(i+1)+"\n"+"Crust:"+ pizzaList.get(i).getCrust()+"\n"+"Size: "+pizzaList.get(i).getSize()+"\n"+"Toppings: "+pizzaList.get(i).getToppings()+"\n"+"Cost:"+pizzaList.get(i).costOfPizza());
+         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+      }
+   }
 }
-		
--- Pizza.java
+++ Pizza.java
@@ -1,4 +1,5 @@
-package lab2;
+// Kody Ellis Pizza.java Tuskegee University Database CS 340 class, Submission date: Feb5, 2018
+
 
 //for collections, comparable, etc
 import java.util.*;
@@ -6,230 +7,233 @@ import java.util.*;
 //Comparable interface used for sorting by Pizza cost
 public class Pizza implements Comparable<Pizza>{
 
-	private final double SMALL = 9.5;
-	private final double MEDIUM = 12.25;
-	private final double  LARGE  = 14.5;
-	private final double TOPPING = 1.5; 
-	
-	private static final double TAX = 0.10;
-	private static double totalCost = 0;
-	
-	private String crust; // hand-tossed, thin, thick
-	private String size; //small, medium, large
-	private String[] toppings; //pepperoni, beef, mushrooms, green pepper, tomato, olive, and sausage 
-	private String toppingList;
-	
-	private double cost;
-	
-	/**
-	 * Initializes a Pizza object
-	 * @param crust This is a string for the crust of the pizza
-	 * Should be hand-tossed, thick, or thin. Not case sensitive
-	 * @param size This is a string for the size of the pizza
-	 * Should be small, medium, or large. Not case sensitive
-	 * */
-	public Pizza(String crust, String size) {
-		//assignment 2 pizza = new assignment2();
-		//crust to lowercase doesnt work in constructor
-		this.crust = crust;
-		this.size = size;
-		
-		this.toppings = new String[7];
-		toppingList = "";
-		
-		cost = 0;
+   private final double SMALL = 9.5;
+   private final double MEDIUM = 12.25;
+   private final double  LARGE  = 14.5;
+   private final double TOPPING = 1.5;
 
-	}
-	
-	/**
-	 * Sets a new String value for string instance variable crust
-	 * @param crust This is a string for the size of the pizza
-	 * Should be hand-tossed, thick, or thin. Not case sensitive
-	 * */
-	public void setCrust(String crust) {
-		this.crust = crust;
-	}
-	
-	/**
-	 * Sets a new String value for string instance variable size
-	 * @param size This is a String for the size of the pizza
-	 * Should be small, medium, large. Not case sensitive
-	 * */
-	public void setSize(String size) {
-		this.size = size;
-	}
-	
+   private static final double TAX = 0.10;
+   private static double totalCost = 0;
 
-	
-	//use for loop to add toppings
-	//remake code so we can easily add them without referencing index number
-	
-	// so make it so that implementation code add each toppping, with a for loop in implemetaiton file
-	//and hopefully they add them sequentially or.........
-	
-	//every time you call this it will search the loop for null...first occurence of null
-	//it replaces that space with the topping
-	
-	/**
-	 * This sets the elements inside the toppings array with actual String elements
-	 * containing topping details
-	 * Before this is set, topping array might contain only null elements.
-	 * @param toppings This is a String array containing string elements
-	 * of toppings for the pizza instance.
-	 * */
-	public void setToppings(String[] toppings) {
-		
-		int index = 0;
-		
-		while (index < 7 && toppings[index] != null) {
-			this.toppings[index] = toppings[index];
-			index++;
-		}
-	}
-	
-	/**
-	 * Adds the parameter cost to the totalCost class variable
-	 * @param cost This is a double variable representing a Pizza's cost
-	 * */
-	public static void setTotal(double cost) {
-		totalCost += cost;
-	}
-	
-	/**
-	 * This returns the total cost of the pizza
-	 * pizza class to use it.
-	 * @returns the class variable total cost
-	 * */
-	public static double getTotal() {
-		return totalCost;
-	}
-	
-	/**
-	 * This returns Tax constant variable
-	 * @return a double class constant variable tax
-	 * */
-	public static double getTax() {
-		return TAX;
-	}
-	
-	/***/
-	public String getCrust() {
-		return crust;
-	}
-	
-	/***/
-	public String getSize() {
-		return size;
-	}
-	
-	/**
-	 *  Concatenates all non-null topping string element
-	 *   to a string then returns the string
-	 * @return List of toppings with possible commas in string data type
-	 * */
+   private String crust; //hand-tossed, thin, thick
+   private String size; //small, medium, large
+   private String[] toppings; //pepperoni, beef, mushrooms, green pepper, tomato, olive, and sausage
+   private String toppingList;
+
+   private double cost;
+
+   /**
+    * Initializes a Pizza object
+    * @param crust This is a string for the crust of the pizza
+    * Should be hand-tossed, thick, or thin. Not case sensitive
+    * @param size This is a string for the size of the pizza
+    * Should be small, medium, or large. Not case sensitive
+    * */
+   public Pizza(String crust, String size) {
+      //assignment 2 pizza = new assignment2();
+      //crust to lowercase doesnt work in constructor
+      this.crust = crust;
+      this.size = size;
+
+      this.toppings = new String[7];
+      toppingList = "";
+
+      cost = 0;
+
+   }
+
+   /**
+    * Sets a new String value for string instance variable crust
+    * @param crust This is a string for the size of the pizza
+    * Should be hand-tossed, thick, or thin. Not case sensitive
+    * */
+   public void setCrust(String crust) {
+      this.crust = crust;
+   }
+
+   /**
+    * Sets a new String value for string instance variable size
+    * @param size This is a String for the size of the pizza
+    * Should be small, medium, large. Not case sensitive
+    * */
+   public void setSize(String size) {
+      this.size = size;
+   }
+
+
+   /**
+    * This sets the elements inside the toppings array with actual String elements
+    * containing topping details
+    * Before this is set, topping array might contain only null elements.
+    * @param toppings This is a String array containing string elements
+    * of toppings for the pizza instance.
+    * */
+   public void setToppings(String[] toppings) {
+
+      int index = 0;
+
+      /*
+         every time you call this it will search the loop for null...first occurence of null
+         it replaces that space with the topping
+      */
+      while (index < 7 && toppings[index] != null) {
+         this.toppings[index] = toppings[index];
+         index++;
+      }
+   }
+
+
+   /**
+    * Adds the parameter cost to the totalCost class variable
+    * @param cost This is a double variable representing a Pizza's cost
+    * */
+   public static void setTotal(double cost) {
+      totalCost += cost;
+   }
+
+
+   /**
+    * This returns the total cost of the pizza
+    * pizza class to use it.
+    * @return the class variable total cost
+    * */
+   public static double getTotal() {
+      return totalCost;
+   }
+
+
+   /**
+    * This returns Tax constant variable
+    * @return a double class constant variable tax
+    * */
+   public static double getTax() {
+      return TAX;
+   }
+
+
+   /**
+    *Returns crust varible
+    * @return a string class instance variable
+    * */
+   public String getCrust() {
+      return crust;
+   }
+
+
+   /**
+    *Returns pizza size variable
+    * @return a string class instance variable
+    * */
+   public String getSize() {
+      return size;
+   }
+
+
+   /**
+    *  Concatenates all non-null topping string element
+    *   to a string then returns the string
+    * @return List of toppings with possible commas in string data type
+    * */
     public String getToppings() {
-		if (toppingList == "") {
-			
-			int i = 0;
-			while (i < 7 && toppings[i] != null) {
-			
-				if (toppingList != "") {
-					toppingList = toppingList + ", " + toppings[i];
+      if (toppingList == "") {
 
-				}else {
-					toppingList = toppingList + toppings[i];
-				}
-				
-				i++;
-			}
-		}
-		
-		return toppingList;
+         int i = 0;
+         while (i < 7 && toppings[i] != null) {
+
+            if (toppingList != "") {
+               toppingList = toppingList + ", " + toppings[i];
+
+            }else {
+               toppingList = toppingList + toppings[i];
+            }
+
+            i++;
+         }
+      }
+
+      return toppingList;
     }
-	
+
     /**
      * Calculates cost of Pizza based on size and amount of toppings
      * @return the cost of the pizza
      * */
-	public double costOfPizza () {
-		//double cost = 0;
-		int toppingsAmount = 0;
-		int index = 0;
-		size = size.toLowerCase();
-		crust = crust.toLowerCase();
-		
-		if (cost == 0) {
-		
-			if (size.equals("large")) {
-				cost += LARGE;
-			}
-			else if(size.equals("medium")) {
-				cost += MEDIUM;
-				
-			}else if(size.equals("small")) {
-				cost += SMALL;
-			}
-			
-			while(index < 7 && toppings[index] != null) {
-				toppingsAmount++;
-				index++;
-			}
-			
-			cost += (toppingsAmount*TOPPING);
-		}
-		//return 0.00;
-		
-		return cost;
-	}
-	
-	
-	/**
-	 * Method from the comparable interface
-	 * returns whether or not an object is less than, equal to, or greater than 
-	 * the specified object
-	 * @param comparePizza a Pizza object that is passed as an argument
-	 * @return a negative integer is object is less than specified object
-	 * 	       a zero is the object is equal to the specified object
-	 *         a positive integer is object is greater than the specified object. 
-	 * */
-	//class Sorting implements Comparator<Pizza>
-	public int compareTo(Pizza comparePizza) {
-		
-		double compareQuantity = ((Pizza) comparePizza).costOfPizza();
-			
-		double intCast = this.cost - compareQuantity;
-		
-		//needs to be int casted as this functions returns int
-		return (int) intCast;
-			
-	}
-		
+   public double costOfPizza () {
+      int toppingsAmount = 0;
+      int index = 0;
+      size = size.toLowerCase();
+      crust = crust.toLowerCase();
 
-	/**
-	 * From the Comparator interface. Uses Comparable interface
-	 * */
-	public static Comparator<Pizza> PizzaCompare = new Comparator<Pizza>() {
-			
-		/**
-		 * @param the first object to be compared.
-		 * @param the second object to be compared.
-		 * @return a negative integer, zero, or a positive integer as 
-		 * the first argument is less than, equal to, or greater than 
-		 * the second.
-		 * @throws NullPointerException - if an argument is null and this 
-		 * comparator does not permit null arguments
-		 * @throws  ClassCastException - if the arguments' types prevent them 
-		 * from being compared by this comparator.
-		 * */
-		public int compare(Pizza a, Pizza b) {
-				
-			//Double ab = a.costOfPizza();
-			//Double ba = b.costOfPizza();
-				
-			return b.compareTo(a);
-		}
-	};
-		
-	
+      if (cost == 0) {
+
+         if (size.equals("large")) {
+            cost += LARGE;
+         }
+         else if(size.equals("medium")) {
+            cost += MEDIUM;
+
+         }else if(size.equals("small")) {
+            cost += SMALL;
+         }
+
+         while(index < 7 && toppings[index] != null) {
+            toppingsAmount++;
+            index++;
+         }
+
+         cost += (toppingsAmount*TOPPING);
+      }
+
+      return cost;
+   }
+
+
+   /**
+    * Method from the comparable interface
+    * returns whether or not an object is less than, equal to, or greater than
+    * the specified object
+    * @param comparePizza a Pizza object that is passed as an argument
+    * @return a negative integer is object is less than specified object
+    *           a zero is the object is equal to the specified object
+    *         a positive integer is object is greater than the specified object.
+    * */
+   //class Sorting implements Comparator<Pizza>
+   public int compareTo(Pizza comparePizza) {
+
+      double compareQuantity = ((Pizza) comparePizza).costOfPizza();
+
+      double intCast = this.cost - compareQuantity;
+
+      //needs to be int casted as this functions returns int
+      return (int) intCast;
+
+   }
+
+
+   /**
+    * From the Comparator interface. Uses Comparable interface
+    * */
+   public static Comparator<Pizza> PizzaCompare = new Comparator<Pizza>() {
+
+      /**
+       * @param the first object to be compared.
+       * @param the second object to be compared.
+       * @return a negative integer, zero, or a positive integer as
+       * the first argument is less than, equal to, or greater than
+       * the second.
+       * @throws NullPointerException - if an argument is null and this
+       * comparator does not permit null arguments
+       * @throws  ClassCastException - if the arguments' types prevent them
+       * from being compared by this comparator.
+       * */
+      public int compare(Pizza a, Pizza b) {
+
+         //Double ab = a.costOfPizza();
+         //Double ba = b.costOfPizza();
+
+         return b.compareTo(a);
+      }
+   };
+
+
 }
-
